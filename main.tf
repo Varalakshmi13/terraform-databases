3e0870f312b2764fddd770c20aedfcff4566e609
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-07-24 07:24:22 +0530

    Updating terraform-databases

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-//module "mongodb" {
-//  source = "./vendor/modules/mongodb"
-//  ENV = var.ENV
-//  WORKSTATION_IP = var.WORKSTATION_IP
-//}
+module "mongodb" {
+  source = "./vendor/modules/mongodb"
+  ENV = var.ENV
+  WORKSTATION_IP = var.WORKSTATION_IP
+  DOCTDB_PORT = var.DOCTDB_PORT
+  DOCDB_INSTANCE_CLASS = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT = var.DOCDB_INSTANCE_COUNT
+
+}
 
 module "redis" {
   source = "./vendor/modules/redis"
@@ -13,6 +17,10 @@ module "mysql" {
   source = "./vendor/modules/mysql"
   ENV = var.ENV
   WORKSTATION_IP = var.WORKSTATION_IP
+  RDS_MYSQL_PORT = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE = var.RDS_MYSQL_STORAGE
+  RDS_ENGINE_VERSION = var.RDS_ENGINE_VERSION
+  RDS_INSTANCE_TYPE = var.RDS_INSTANCE_TYPE
 }
 
 module "rabbitmq" {
--- vars.tf
+++ vars.tf
@@ -1,2 +1,12 @@
 variable "ENV" {}
-variable WORKSTATION_IP {}
+variable "WORKSTATION_IP" {}
+
+variable "RDS_MYSQL_PORT"  {}
+variable "RDS_MYSQL_STORAGE"  {}
+variable "RDS_ENGINE_VERSION"  {}
+variable "RDS_INSTANCE_TYPE"  {}
+
+variable "DOCTDB_PORT" {}
+variable "DOCDB_INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_COUNT" {}
+
